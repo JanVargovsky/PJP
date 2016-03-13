5e8e1763bee217da55725066f2b6838673a2e7d2
Jan Vargovský <jan.vargovsky@gmail.com>
2016-03-13 20:06:36 +0100

    Strassen multiplies normally now. NOTE: FML

--- MatrixMultiplication/Source.cpp
+++ MatrixMultiplication/Source.cpp
@@ -8,12 +8,12 @@
 using namespace std;
 
 int USE_STRASSEN = 1;
-int USE_PARALLEL = 1;
+int USE_PARALLEL = 0;
 
 class Matrix
 {
 private:
-	const int MIN_SIZE_TO_USE_STRASSEN = 128;
+	const int MIN_SIZE_TO_USE_STRASSEN = 4;
 
 	const int rows, columns;
 	const int size;
@@ -36,7 +36,6 @@ private:
 		// TODO: add validation
 		Matrix * result = new Matrix(rowTo - rowFrom, colTo - colFrom);
 
-
 		for (int row = rowFrom, i = 0; row < rowTo; row++, i++)
 #pragma omp parallel for
 			for (int col = colFrom, j = 0; col < colTo; col++, j++)
@@ -54,14 +53,26 @@ private:
 		int rowShift = a11.rows;
 		int colShift = a11.columns;
 
+		//cout << "a11" << endl;
+		//cout << a11.ToString() << endl;
+		//cout << "a12" << endl;
+		//cout << a12.ToString() << endl;
+		//cout << "a21" << endl;
+		//cout << a21.ToString() << endl;
+		//cout << "a22" << endl;
+		//cout << a22.ToString() << endl;
+
 		for (int row = 0; row < a11.rows; row++)
 			for (int col = 0; col < a11.columns; col++)
 			{
 				result->Set(row, col, a11.Get(row, col));
 				result->Set(row, col + colShift, a12.Get(row, col));
 				result->Set(row + rowShift, col, a21.Get(row, col));
-				result->Set(row + rowShift, col, a22.Get(row, col));
+				result->Set(row + rowShift, col + colShift, a22.Get(row, col));
 			}
+
+		//cout << "COMBINED" << endl;
+		//cout << result->ToString() << endl;
 
 		return result;
 	}
@@ -140,6 +151,28 @@ public:
 	inline int Index(int row, int col) const
 	{
 		return row * columns + col;
+	}
+
+	string ToString()
+	{
+		stringstream ss;
+		for (int row = 0; row < GetRows(); row++)
+		{
+			for (int col = 0; col < GetColumns(); col++)
+				ss << Get(row, col) << " ";
+			ss << endl;
+		}
+		return ss.str();
+	}
+
+	bool AreEqual(Matrix & other)
+	{
+		for (int row = 0; row < GetRows(); row++)
+			for (int col = 0; col < GetColumns(); col++)
+				if (Get(row, col) != other.Get(row, col))
+					return false;
+
+		return true;
 	}
 #pragma endregion
 
@@ -322,49 +355,73 @@ public:
 			return this->NormalMultiplyParallel(other);
 
 		int halfN = n / 2;
-
 		auto a11 = SubMatrixParallel(0, halfN, 0, halfN);
 		auto a12 = SubMatrixParallel(0, halfN, halfN, n);
 		auto a21 = SubMatrixParallel(halfN, n, 0, halfN);
 		auto a22 = SubMatrixParallel(halfN, n, halfN, n);
 
-		auto b11 = SubMatrixParallel(0, halfN, 0, halfN);
-		auto b12 = SubMatrixParallel(0, halfN, halfN, n);
-		auto b21 = SubMatrixParallel(halfN, n, 0, halfN);
-		auto b22 = SubMatrixParallel(halfN, n, halfN, n);
+		auto b11 = other.SubMatrixParallel(0, halfN, 0, halfN);
+		auto b12 = other.SubMatrixParallel(0, halfN, halfN, n);
+		auto b21 = other.SubMatrixParallel(halfN, n, 0, halfN);
+		auto b22 = other.SubMatrixParallel(halfN, n, halfN, n);
 
-		auto m1_a = (*a11 + *a22);
-		auto m1_b = (*b11 + *b22);
-		auto m2_a = *a21 + *a22;
-		auto m3_b = (*b12 - *b22);
-		auto m4_b = (*b21 - *b11);
-		auto m5_a = *a11 + *a12;
-		auto m6_a = *a21 - *a11;
-		auto m6_b = *b11 + *b12;
-		auto m7_a = *a12 - *a22;
-		auto m7_b = *b21 + *b22;
+		auto m1_a = *a11 + *a22;  auto m1_b = *b11 + *b22;
+		auto m2_a = *a21 + *a22;  auto m2_b = b11;
+		auto m3_a = a11;		  auto m3_b = *b12 - *b22;
+		auto m4_a = a22;		  auto m4_b = *b21 - *b11;
+		auto m5_a = *a11 + *a12;  auto m5_b = b22;
+		auto m6_a = *a21 - *a11;  auto m6_b = *b11 + *b12;
+		auto m7_a = *a12 - *a22;  auto m7_b = *b21 + *b22;
+
+		//Matrix* m_STRASSEN[7] = {
+		//	m1_a->StrassenMultiplyParallel(*m1_b), // m1
+		//	m2_a->StrassenMultiplyParallel(*m2_b), // m2
+		//	m3_a->StrassenMultiplyParallel(*m3_b), // m3
+		//	m4_a->StrassenMultiplyParallel(*m4_b), // m4
+		//	m5_a->StrassenMultiplyParallel(*m5_b), // m5
+		//	m6_a->StrassenMultiplyParallel(*m6_b), // m6
+		//	m7_a->StrassenMultiplyParallel(*m7_b), // m7
+		//};
 
 		Matrix* m[7] = {
 			m1_a->StrassenMultiplyParallel(*m1_b), // m1
-			m2_a->StrassenMultiplyParallel(*b11), // m2
-			a11->StrassenMultiplyParallel(*m3_b), // m3
-			a22->StrassenMultiplyParallel(*m4_b), // m4
-			m5_a->StrassenMultiplyParallel(*b22), // m5
+			m2_a->StrassenMultiplyParallel(*m2_b), // m2
+			m3_a->StrassenMultiplyParallel(*m3_b), // m3
+			m4_a->StrassenMultiplyParallel(*m4_b), // m4
+			m5_a->StrassenMultiplyParallel(*m5_b), // m5
 			m6_a->StrassenMultiplyParallel(*m6_b), // m6
 			m7_a->StrassenMultiplyParallel(*m7_b), // m7
 		};
 
+		//for (size_t i = 0; i < 7; i++)
+		//{
+		//	if (!m[i]->AreEqual(*m_STRASSEN[i]))
+		//	{
+		//		if (i == 1)
+		//		{
+		//			cout << *m2_a << endl;
+		//			cout << "*" << endl;
+		//			cout << *m2_b << endl;
+		//		}
+		//		cout << "STRASSEN M" << (i + 1) << endl;
+		//		cout << *m_STRASSEN[i] << endl;
+		//		cout << "CORRECT M" << (i + 1) << endl;
+		//		cout << *m[i] << endl;
+		//	}
+		//}
+
+		//cout << endl;
+
 		auto c11_l = (*m[0] + *m[3]);
-		auto c11_r = (*m[4] + *m[6]);
+		auto c11_r = (*m[6] - (*m[4]));
 
 		auto c22_l = (*m[0] - *m[1]);
 		auto c22_r = (*m[2] + *m[5]);
 
-		auto c11 = *c11_l - *c11_r;
+		auto c11 = *c11_l + *c11_r;
 		auto c12 = *m[2] + *m[4];
 		auto c21 = *m[1] + *m[3];
 		auto c22 = *c22_l + *c22_r;
-
 
 		auto result = CombineSubMatrices(*c11, *c12, *c21, *c22);
 
@@ -401,6 +458,7 @@ public:
 		delete c22;
 
 		return result;
+
 	}
 #pragma endregion
 };
@@ -431,11 +489,6 @@ Matrix * RandomMatrix(int rows, int cols)
 	return matrix;
 }
 
-void PrintMeasureResult(string message, double start, double end)
-{
-	cout << message << ": " << (end - start) << "s" << endl;
-}
-
 void RunTest(Matrix * a, Matrix * b, int strassen, int parallel)
 {
 	USE_STRASSEN = strassen;
@@ -445,7 +498,10 @@ void RunTest(Matrix * a, Matrix * b, int strassen, int parallel)
 	auto end = omp_get_wtime();
 	stringstream ss;
 	ss << "Strassen = " << strassen << " Parallel = " << parallel << " TIME";
-	PrintMeasureResult(ss.str(), start, end);
+	cout << ss.str() << ": " << (end - start) << "s" << endl;
+	//cout << "A" << endl << *a << endl;
+	//cout << "B" << endl << *b << endl;
+	//cout << "C" << endl << *c << endl;
 	delete c;
 }
 
@@ -453,26 +509,24 @@ int main()
 {
 #ifdef _DEBUG
 	FILE *stream;
-	freopen_s(&stream, "in.txt", "r", stdin);
-	freopen_s(&stream, "out.txt", "w", stdout);
+	//freopen_s(&stream, "in.txt", "r", stdin);
+	//freopen_s(&stream, "out.txt", "w", stdout);
 #endif
 	omp_set_num_threads(omp_get_max_threads());
 
-	const int SIZE = 2048;
+	const int SIZE = 1024;
 	double start = omp_get_wtime();
 	auto a = RandomMatrix(SIZE, SIZE);
 	double end = omp_get_wtime();
-	PrintMeasureResult("Generating A", start, end);
 
 	start = omp_get_wtime();
 	auto b = RandomMatrix(SIZE, SIZE);
 	end = omp_get_wtime();
-	PrintMeasureResult("Generating B", start, end);
 
 	RunTest(a, b, 1, 1); // parallel strassen
-	//RunTest(a, b, 1, 0); // normal strassen
-	//RunTest(a, b, 0, 1); // parallel multiplication
-	//RunTest(a, b, 0, 0); // normal multiplication
+	RunTest(a, b, 1, 0); // normal strassen
+	RunTest(a, b, 0, 1); // parallel multiplication
+	RunTest(a, b, 0, 0); // normal multiplication
 
 	delete a;
 	delete b;
